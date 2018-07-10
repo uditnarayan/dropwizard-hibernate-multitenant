1ab27987e69700862578a50e6a3335a90d9e8592
Udit Narayan <info.udit90@gmail.com>
2018-07-10 12:06:43 +0530

    Provides properties in Tenant

--- src/main/java/io/dropwizard/hibernate/multitenant/Tenant.java
+++ src/main/java/io/dropwizard/hibernate/multitenant/Tenant.java
@@ -2,25 +2,73 @@ package io.dropwizard.hibernate.multitenant;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.db.DataSourceFactory;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
-@NoArgsConstructor
-@AllArgsConstructor
 public final class Tenant {
 
-    @Getter
     @JsonProperty
     private String id;
 
-    @Getter
     @JsonProperty
     private Map<String, String> properties;
 
-    @Getter
     @JsonProperty
     private Map<String, DataSourceFactory> databases;
+
+    public Tenant() {
+        this.properties = new HashMap<>();
+        this.databases = new HashMap<>();
+    }
+
+    public Tenant(String id, Map<String, String> properties, Map<String, DataSourceFactory> databases) {
+        this.id = id;
+        this.properties = properties;
+        this.databases = databases;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    public Map<String, DataSourceFactory> getDatabases() {
+        return databases;
+    }
+
+    public boolean getBooleanProperty(String property) {
+        return Boolean.parseBoolean(this.properties.get(property));
+    }
+
+    public int getIntegerProperty(String property) {
+        return Integer.parseInt(this.properties.get(property));
+    }
+
+    public float getFloatProperty(String property) {
+        return Float.parseFloat(this.properties.get(property));
+    }
+
+    public char getCharacterProperty(String property) {
+        return this.properties.get(property).charAt(0);
+    }
+
+    public double getDoubleProperty(String property) {
+        return Double.parseDouble(this.properties.get(property));
+    }
+
+    public String getStringProperty(String property) {
+        return this.properties.get(property);
+    }
+
+    public Date getDateProperty(String property, SimpleDateFormat format) throws ParseException {
+        String dateString = this.properties.getOrDefault(property, null);
+        return format.parse(dateString);
+    }
 }
